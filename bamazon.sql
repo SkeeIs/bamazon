8e1be5c66c28a53db0ed93eec94a32539478ef12
Taylor Skeels <skeels@Taylors-MacBook-Pro.local>
2018-09-04 14:26:35 -0700

    customer interface working

--- bamazon.sql
+++ bamazon.sql
@@ -8,7 +8,7 @@ CREATE TABLE products (
   item_id INT(10) NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NOT NULL,
   department_name VARCHAR(50) NOT NULL,
-  price DECIMAL(10, 2) NOT NULL,
+  price DECIMAL(5, 2) NOT NULL,
   stock_quantity INT(10) NOT NULL,
   PRIMARY KEY (product_id)
 );
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -8,3 +9,75 @@ var connection = mysql.createConnection({
     password: "password",
     database: "bamazon_DB"
 });
+
+//function to query the SQL db for table values & construct the "storefront"
+function displayShop() {
+    //query SQL for all data from the products table
+    connection.query("SELECT * FROM products", function (err, res) {
+      //if error occurs display the error
+      if (err) throw err;
+      //console.log(res); 
+      //create new table using cli-table
+      var table = new Table({
+          head: ["Item ID", "Product Name", "Department", "Price", "Stock"],
+          colWidths: [10, 45, 18, 10, 10]
+      });
+      //populate cli-table with data from response to our sql query
+      for (var i = 0; i < res.length; i++) {
+        table.push(
+            [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+        );
+      }
+      //print the table to terminal
+      console.log(table.toString());
+      customerPrompts();
+    })
+}
+//function using inquirer package to interact with user
+function customerPrompts() {
+    //use inquirer package to obtain user input for item ID & quantity to purchase
+    inquirer
+    .prompt([{
+        message: "Please enter the Item ID of the product you would like to purchase",
+        type: "input",
+        name: "itemID"
+    },{
+        message: "Please enter the quantity you would like to purchase",
+        type: "input",
+        name: "quantity"
+    }])
+    //promise waiting before continuing on
+    .then(function(userInputs) {
+        //setting user inputs to variables for ease of use 
+        var selectItem = userInputs.itemID;
+        var quantity = parseFloat(userInputs.quantity);
+        
+        connection.query("SELECT * FROM products WHERE ?", {item_id: selectItem}, function(err, res) {
+          if (err) throw err;
+          
+          //console.log(res);
+          //console.log(quantity);
+          //console.log(res[0].stock_quantity);
+          var stock = res[0].stock_quantity;
+          var price = res[0].price;
+          var name = res[0].product_name;
+
+          if (quantity <= stock) {
+            
+            console.log("Product in stock! Processing...")
+
+            connection.query("UPDATE products SET ? WHERE ?", [{stock_quantity: stock - quantity}, {item_id: selectItem}], function(err, res) {
+              if (err) throw err;
+              //console.log(res)  
+              console.log("Order placed! Your Bamazon card has been charged $" + price * quantity + ".");
+              console.log("Your order of " + name + ", in the amount of " + quantity + ", will arrive in two business days. Thanks for shopping Bamazon!");
+            })
+          }
+          else {
+            console.log("Insufficient quantity! Please try again.")
+            displayShop();  
+          } 
+        })
+    })
+}
+displayShop();
